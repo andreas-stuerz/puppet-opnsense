d8709c5a20cc39b93a38d2290f5e1d537332487e
Andreas Stuerz <andreas.stuerz@markt.de>
2021-07-16 17:12:13 +0200

    add acceptance test for opnsense firewall rule type

--- spec/spec_helper_acceptance_local.rb
+++ spec/spec_helper_acceptance_local.rb
@@ -45,6 +45,26 @@ def get_abolute_path_from_spec_dir(rel_path)
   File.join(File.dirname(__FILE__), rel_path)
 end
 
+def install_opnsense_plugin(name)
+  pp_install_plugin = <<-MANIFEST
+    opnsense_plugin { '#{name}':
+      device => 'opnsense-test.device.com'
+      ensure => 'absent',
+    }
+  MANIFEST
+  LitmusHelper.instance.apply_manifest(pp_install_plugin, catch_failures: true)
+end
+
+def uninstall_opnsense_plugin(name)
+  pp_uninstall_plugin = <<-MANIFEST
+    opnsense_plugin { '#{name}':
+      device => 'opnsense-test.device.com'
+      ensure => 'absent',
+    }
+  MANIFEST
+  LitmusHelper.instance.apply_manifest(pp_uninstall_plugin, catch_failures: true)
+end
+
 def setup_test_api_endpoint
   api_config = hash_from_fixture_yaml_file('/acceptance/opn-cli/conf.yaml')
   pp_setup = <<-MANIFEST

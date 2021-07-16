93f00d1b7768f53e761dc37903fddeb4b21ce7a9
Andreas Stuerz <andreas.stuerz@markt.de>
2021-07-16 15:28:16 +0200

    add type for opnsense firewall rule

---
+++ lib/puppet/provider/opnsense_firewall_rule/opnsense_firewall_rule.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'puppet/resource_api/simple_provider'
+require 'json'
+
+# Implementation for the opnsense_firewall_rule type using the Resource API.
+class Puppet::Provider::OpnsenseFirewallRule::OpnsenseFirewallRule < Puppet::Provider::OpnsenseProvider
+  def get(context)
+    context.debug('Returning pre-canned example data')
+    [
+      {
+        name: 'foo',
+        ensure: 'present',
+      },
+      {
+        name: 'bar',
+        ensure: 'present',
+      },
+    ]
+  end
+
+  def create(context, name, should)
+    context.notice("Creating '#{name}' with #{should.inspect}")
+  end
+
+  def update(context, name, should)
+    context.notice("Updating '#{name}' with #{should.inspect}")
+  end
+
+  def delete(context, name)
+    context.notice("Deleting '#{name}'")
+  end
+end

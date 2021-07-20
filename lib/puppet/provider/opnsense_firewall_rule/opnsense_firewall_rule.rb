37a10b0bbd9ad6b8b34a123bd92872fc06f2e9d5
Andreas Stuerz <andreas.stuerz@markt.de>
2021-07-20 15:07:35 +0200

    add firewall rule get

--- lib/puppet/provider/opnsense_firewall_rule/opnsense_firewall_rule.rb
+++ lib/puppet/provider/opnsense_firewall_rule/opnsense_firewall_rule.rb
@@ -5,18 +5,54 @@ require 'json'
 
 # Implementation for the opnsense_firewall_rule type using the Resource API.
 class Puppet::Provider::OpnsenseFirewallRule::OpnsenseFirewallRule < Puppet::Provider::OpnsenseProvider
-  def get(context)
-    context.debug('Returning pre-canned example data')
-    [
-      {
-        name: 'foo',
-        ensure: 'present',
-      },
-      {
-        name: 'bar',
-        ensure: 'present',
-      },
-    ]
+  # @param [Puppet::ResourceApi::BaseContext] _context
+  # @param [Array<Hash<Symbol>>] filter
+  # @return [Array<Hash<Symbol>>]
+  def get(_context, filter)
+    device_names = get_device_names_by_filter(filter)
+    _get_firewall_rules_from_devices(device_names)
+  end
+
+  # @param [Array<String>] devices
+  # @return [Array<Hash<Symbol>>]
+  def _get_firewall_rules_from_devices(devices)
+    result = []
+    devices.each do |device|
+      rules = _rules_list(device)
+      rules.each do |fw_rule|
+        result.push(
+            title: "#{fw_rule['sequence']} #{fw_rule['description']}@#{device}",
+            sequence: fw_rule['sequence'],
+            description: fw_rule['description'],
+            device: device,
+            uuid: fw_rule['uuid'],
+            action: fw_rule['action'],
+            interface: fw_rule['interface'].split(','),
+            direction: fw_rule['direction'],
+            quick: bool_from_value(fw_rule['quick']),
+            ipprotocol: fw_rule['ipprotocol'],
+            protocol: fw_rule['protocol'],
+            source_net: fw_rule['source_net'],
+            source_port: fw_rule['source_port'],
+            source_not:  bool_from_value(fw_rule['source_not']),
+            destination_net: fw_rule['destination_net'],
+            destination_port: fw_rule['destination_port'],
+            destination_not:  bool_from_value(fw_rule['destination_not']),
+            gateway: fw_rule['gateway'],
+            log:  bool_from_value(fw_rule['log']),
+            enabled:  bool_from_value(fw_rule['enabled']),
+            ensure: 'present',
+          )
+      end
+    end
+    result
+  end
+
+  # @param [String] device_name
+  # @return [Array]
+  def _rules_list(device_name)
+    json_output = opn_cli_base_cmd(device_name, ['firewall', 'rule', 'list', '-o', 'json'])
+    JSON.parse(json_output)
   end
 
   def create(context, name, should)
@@ -30,4 +66,6 @@ class Puppet::Provider::OpnsenseFirewallRule::OpnsenseFirewallRule < Puppet::Pro
   def delete(context, name)
     context.notice("Deleting '#{name}'")
   end
+
+  private :_get_firewall_rules_from_devices
 end
--- spec/unit/puppet/provider/opnsense_firewall_rule/opnsense_firewall_rule_spec.rb
+++ spec/unit/puppet/provider/opnsense_firewall_rule/opnsense_firewall_rule_spec.rb
@@ -10,43 +10,89 @@ RSpec.describe Puppet::Provider::OpnsenseFirewallRule::OpnsenseFirewallRule do
 
   let(:context) { instance_double('Puppet::ResourceApi::BaseContext', 'context') }
 
+  let(:rules_device_1) do
+    [
+      {
+        "enabled": '1',
+          "sequence": '1',
+          "action": 'pass',
+          "quick": '1',
+          "interface": 'lan',
+          "direction": 'in',
+          "ipprotocol": 'inet',
+          "protocol": 'TCP',
+          "source_net": 'any',
+          "source_not": '0',
+          "source_port": '',
+          "destination_net": 'any',
+          "destination_not": '0',
+          "destination_port": '',
+          "gateway": '',
+          "log": '0',
+          "description": 'example rule1',
+          "uuid": '624cb3ca-3b76-4177-b736-4381c6525f37'
+      },
+      {
+        "enabled": '1',
+          "sequence": '2',
+          "action": 'block',
+          "quick": '1',
+          "interface": 'lan',
+          "direction": 'in',
+          "ipprotocol": 'inet',
+          "protocol": 'TCP',
+          "source_net": '192.168.61.1',
+          "source_not": '0',
+          "source_port": '',
+          "destination_net": '192.168.70.0/24',
+          "destination_not": '0',
+          "destination_port": 'https',
+          "gateway": '',
+          "log": '0',
+          "description": 'example rule 2',
+          "uuid": '002db5b7-791e-4e2f-8625-4350ee5ae8ac'
+      },
+    ]
+  end
+
   describe '#get' do
-    it 'processes resources' do
-      expect(context).to receive(:debug).with('Returning pre-canned example data')
-      expect(provider.get(context)).to eq [
-        {
-          name: 'foo',
-          ensure: 'present',
-        },
-        {
-          name: 'bar',
-          ensure: 'present',
-        },
-      ]
+    context 'with empty filter' do
+      it 'returns all resources' do
+        expect(provider.get(context)).to eq [
+          {
+            name: 'foo',
+            ensure: 'present',
+          },
+          {
+            name: 'bar',
+            ensure: 'present',
+          },
+        ]
+      end
     end
   end
 
-  describe 'create(context, name, should)' do
-    it 'creates the resource' do
-      expect(context).to receive(:notice).with(%r{\ACreating 'a'})
-
-      provider.create(context, 'a', name: 'a', ensure: 'present')
-    end
-  end
-
-  describe 'update(context, name, should)' do
-    it 'updates the resource' do
-      expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
-
-      provider.update(context, 'foo', name: 'foo', ensure: 'present')
-    end
-  end
-
-  describe 'delete(context, name)' do
-    it 'deletes the resource' do
-      expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
-
-      provider.delete(context, 'foo')
-    end
-  end
+  # describe 'create(context, name, should)' do
+  #   it 'creates the resource' do
+  #     expect(context).to receive(:notice).with(%r{\ACreating 'a'})
+  #
+  #     provider.create(context, 'a', name: 'a', ensure: 'present')
+  #   end
+  # end
+  #
+  # describe 'update(context, name, should)' do
+  #   it 'updates the resource' do
+  #     expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
+  #
+  #     provider.update(context, 'foo', name: 'foo', ensure: 'present')
+  #   end
+  # end
+  #
+  # describe 'delete(context, name)' do
+  #   it 'deletes the resource' do
+  #     expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
+  #
+  #     provider.delete(context, 'foo')
+  #   end
+  # end
 end

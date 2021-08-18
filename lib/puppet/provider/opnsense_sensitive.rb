2abfc899ee404f4d13b6e5ec398b2ec84397ddd4
Andreas Stuerz <andreas.stuerz@markt.de>
2021-08-18 09:31:20 +0200

    add YAML serialization helper for Psych.
    add test for yaml serialisation

--- lib/puppet/provider/opnsense_sensitive.rb
+++ lib/puppet/provider/opnsense_sensitive.rb
@@ -2,14 +2,14 @@
 #
 class Puppet::Provider::OpnsenseSensitive < Puppet::Pops::Types::PSensitiveType::Sensitive
   # @param [Puppet::Pops::Types::PSensitiveType::Sensitive] other
-  # @return [TrueClass]
+  # @return [Boolean]
   def ==(other)
     return true if other.is_a?(Puppet::Pops::Types::PSensitiveType::Sensitive) && unwrap == other.unwrap
   end
 
   # The transactionstore uses psych to dump yaml to the cache file.
   # This lets us control how that is serialized.
-  # @param [Hash] coder
+  # @param [Psych::Coder] coder
   def encode_with(coder)
     coder.tag = nil
     coder.scalar = 'Puppet::Provider::OpnsenseSensitive <<encrypted>>'

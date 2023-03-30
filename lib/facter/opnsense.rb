6bcf92811f93d560829d41adbd2e3d66871f79d6
Andreas Stuerz <andreas.stuerz@markt.de>
2023-03-30 15:59:50 +0200

    update vagrant box

--- lib/facter/opnsense.rb
+++ lib/facter/opnsense.rb
@@ -2,8 +2,8 @@ Facter.add(:opnsense) do
   confine kernel: 'FreeBSD'
   setcode do
     opnsense_version = Facter::Core::Execution.which('opnsense-version')
-    if opnsense_version.blank?
-      return
+    if opnsense_version.nil?
+      next
     end
 
     facts = {}
